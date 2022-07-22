9794993371954db850e4a8649a80291c8ad02232
Matheus Victor <matheusvictor.salles@gmail.com>
2022-07-22 19:34:19 -0300

    Add classe de modelo e repositório

---
+++ src/main/kotlin/br/com/curso/udemy/model/Cliente.kt
@@ -0,0 +1,17 @@
+package br.com.curso.udemy.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Cliente(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+    val nome: String,
+    val documento: String,
+    val endereco: String
+
+)
