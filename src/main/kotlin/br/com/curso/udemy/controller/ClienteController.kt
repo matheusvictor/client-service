84b50765ad15238b0734d1c7bdf60d3db81474cf
Matheus Victor <matheusvictor.salles@gmail.com>
2022-08-02 11:49:32 -0300

    update: Modifica endpoint de cadastro de recurso

--- src/main/kotlin/br/com/curso/udemy/controller/ClienteController.kt
+++ src/main/kotlin/br/com/curso/udemy/controller/ClienteController.kt
@@ -1,19 +1,30 @@
 package br.com.curso.udemy.controller
 
 import br.com.curso.udemy.model.Cliente
+import br.com.curso.udemy.request.ClienteRequest
 import br.com.curso.udemy.service.ClienteService
 import io.micronaut.data.model.Page
 import io.micronaut.data.model.Pageable
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.*
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/clientes")
+// Essa classe vai funcionar como um DTO
 class ClienteController(
-        private val service: ClienteService
+    private val service: ClienteService
 ) {
 
     @Post
-    fun cadastrar(@Body cliente: Cliente): HttpResponse<Cliente> {
+    fun cadastrar(@Body @Valid clienteRequest: ClienteRequest): HttpResponse<Cliente> {
+
+        // println(clienteRequest) // imprimi os dados recebidos na requisição
+
+        val cliente: Cliente = clienteRequest.fromCliente() // converte a requisição (DTO) para obj. de domínio
+        // println(cliente.nome) // imprimi o nome do objeto covertido
+
         val clienteDB: Cliente = service.cadastrar(cliente)
         return HttpResponse.created(clienteDB)
     }
